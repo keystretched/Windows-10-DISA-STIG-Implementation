1501a838b3462ae534493112a94f584fc08b91bd
keystretched <bwaddell77@gmail.com>
2025-09-08 03:26:16 -0500

    Create WN10-AC-000020.ps1

---
+++ Windows_10_DISA_STIGS/WN10-AC-000020.ps1
@@ -0,0 +1,31 @@
+<#
+.SYNOPSIS
+    This PowerShell script prevents reuse of recently used passwords to enhance account security.
+
+.NOTES
+    Author          : Brandon Waddell
+    LinkedIn        : linkedin.com
+    GitHub          : github.com/keystreched
+    Date Created    : 2025-09-08
+    Last Modified   : 2025-09-08
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-AC-000020
+
+.TESTED ON
+    Date(s) Tested  : 2025-06-09
+    Tested By       : 2025-06-09
+    Systems Tested  : 2025-06-09
+    PowerShell Ver. : 2025-06-09
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\STIG-ID-WN10-AC-000020.ps1 
+#>
+
+# WN10-AC-000020 - Enforce Password History (24 remembered)
+
+# Configure system to remember the last 24 passwords for each local user account
+net accounts /uniquepw:24
