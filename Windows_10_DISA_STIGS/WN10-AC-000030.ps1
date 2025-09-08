2c42772bcfe658d4002b7dc403fdd1d98de3ade0
keystretched <bwaddell77@gmail.com>
2025-09-08 03:29:59 -0500

    Create WN10-AC-000030.ps1

---
+++ Windows_10_DISA_STIGS/WN10-AC-000030.ps1
@@ -0,0 +1,31 @@
+<#
+.SYNOPSIS
+    This PowerShell script prevents users from immediately cycling through passwords to reuse an old one.
+
+.NOTES
+    Author          : Brandon Waddell
+    LinkedIn        : linkedin.com
+    GitHub          : github.com/keystreched
+    Date Created    : 2025-09-08
+    Last Modified   : 2025-09-08
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-AC-000030
+
+.TESTED ON
+    Date(s) Tested  : 2025-06-09
+    Tested By       : 2025-06-09
+    Systems Tested  : 2025-06-09
+    PowerShell Ver. : 2025-06-09
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\STIG-ID-WN10-AC-000030.ps1 
+#>
+
+# WN10-AC-000030 - Minimum Password Age = 1
+
+# Set minimum password age to 1 day for local accounts
+net accounts /minpwage:1
